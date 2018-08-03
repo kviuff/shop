6faa265768189aee98d4bcadff61918a4c929382
kviuff <kanglan@leimingtech.com>
2018-08-03 18:02:29 +0800

    角色、用户

--- src/main/java/com/kviuff/shop/controller/user/SysUserRestController.java
+++ src/main/java/com/kviuff/shop/controller/user/SysUserRestController.java
@@ -1,8 +1,16 @@
 package com.kviuff.shop.controller.user;
 
 
+import com.github.pagehelper.PageInfo;
+import com.kviuff.shop.common.entity.SysMenuPo;
+import com.kviuff.shop.common.entity.SysUserPo;
+import com.kviuff.shop.common.utils.R;
+import com.kviuff.shop.service.user.SysUserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * 用户接口-restful
@@ -14,4 +22,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/rest/sys/user")
 public class SysUserRestController {
 
+    @Autowired
+    private SysUserService sysUserService;
+
+    /**
+     * 获取用户分页数据
+     *
+     * @return
+     */
+    @RequestMapping("/list")
+    public R findAllMenu() {
+        SysUserPo sysUserPo = new SysUserPo();
+        PageInfo<SysUserPo> sysUserPoPageInfo = sysUserService.findPageList(sysUserPo);
+        return R.ok().put("data", null);
+    }
 }
